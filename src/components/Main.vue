9261e4c9f162cca6ff591c1620368098533efed0
Ishan Manandhar <ishan02016@gmail.com>
2020-05-31 14:23:04 +0545

    added static data source

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,11 @@
     <h5 class="text-left">{{ title }}</h5>
     <br />
     <div class="row">
-      <div class="col-sm-4">
+      <div
+        class="col-sm-4"
+        v-for="(podcasters, index) in podcasters"
+        :key="index"
+      >
         <div class="card" style="width: 100%;">
           <img
             src="http://via.placeholder.com/348x196"
@@ -11,79 +15,14 @@
             alt="image"
           />
           <div class="card-body text-left">
-            <small>Design</small>
-            <h5 class="card-title">Design Better Podcast</h5>
+            <small>{{podcasters.category}}</small>
+            <h5 class="card-title">{{podcasters.heading}}</h5>
             <span>
-              <img src="@/assets/mic.svg" class="mic" alt="image" />Aarron
-              Walter and Eli Woolery</span
-            >
-            <b-button v-b-modal.modalDisplay variant="primary"
-              ><svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="15"
-                height="15"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-plus"
-              >
-                <line x1="12" y1="5" x2="12" y2="19"></line>
-                <line x1="5" y1="12" x2="19" y2="12"></line></svg
-              >&nbsp;Follow</b-button
-            >
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="card" style="width: 100%;">
-          <img
-            src="http://via.placeholder.com/348x196"
-            class="card-img-top"
-            alt="image"
-          />
-          <div class="card-body text-left">
-            <small>Programming</small>
-            <h5 class="card-title">Software Engineering Radio</h5>
-            <span>
-              <img src="@/assets/mic.svg" class="mic" alt="image" />Thomas
-              Houston</span
-            >
-            <b-button v-b-modal.modalDisplay variant="primary"
-              ><svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="15"
-                height="15"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-plus"
-              >
-                <line x1="12" y1="5" x2="12" y2="19"></line>
-                <line x1="5" y1="12" x2="19" y2="12"></line></svg
-              >&nbsp;Follow</b-button
-            >
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="card" style="width: 100%;">
-          <img
-            src="http://via.placeholder.com/348x196"
-            class="card-img-top"
-            alt="image"
-          />
-          <div class="card-body text-left">
-            <small>Programming</small>
-            <h5 class="card-title">Javascript Jabber</h5>
-            <span>
-              <img src="@/assets/mic.svg" class="mic" alt="image" />Jabber
-              Media</span
+              <img
+                src="@/assets/mic.svg"
+                class="mic"
+                alt="image"
+              />{{podcasters.host}}</span
             >
             <b-button v-b-modal.modalDisplay variant="primary"
               ><svg
@@ -113,10 +52,18 @@
 </template>
 
 <script>
+import podcasters from '@/data/podcasters';
+
 export default {
   name: 'Main',
   props: {
     title: String
+  },
+  data() {
+    console.log(podcasters);
+    return {
+      podcasters
+    };
   }
 };
 </script>
@@ -141,7 +88,7 @@ button.btn.btn-primary {
   margin-top: 20px;
   display: table;
 }
-button.btn.btn-primary:focus{
+button.btn.btn-primary:focus {
   outline: none;
   box-shadow: none;
 }
---
+++ src/data/podcasters.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    category: 'Design',
+    heading: 'Design Better Podcast',
+    host: 'Aarron Walter and Eli Woolery'
+  },
+  {
+    category: 'Programming',
+    heading: 'Software Engineering Radio',
+    host: 'Thomas Houston'
+  },
+  {
+    category: 'Programming',
+    heading: 'Javascript Jabber',
+    host: 'Jabber Media'
+  }
+];
